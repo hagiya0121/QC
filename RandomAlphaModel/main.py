d90b39f4adf07ff27dc8cdc9e35bc93cab0f5859
hagiya0121 <koazu0121@gmail.com>
2025-06-30 12:13:10 +0900

    RandomAlphaModelをライブラリ化

--- RandomAlphaModel/main.py
+++ RandomAlphaModel/main.py
@@ -1,6 +1,5 @@
-from AlgorithmImports import *  # type: ignore[reportWildcardImportFromLibrary]
-from datetime import timedelta
-import random
+from AlgorithmImports import *
+from CustomAlphaModel import RandomAlphaModel
 
 
 class RandomAlphaModelAlgorithm(QCAlgorithm):
@@ -13,35 +12,4 @@ class RandomAlphaModelAlgorithm(QCAlgorithm):
         self.add_future(Futures.Grains.SOYBEANS, Resolution.DAILY)
         self.add_future(Futures.Grains.SOYBEAN_OIL, Resolution.DAILY)
 
-        self.add_alpha(RandomAlphaModel())
-
-
-class RandomAlphaModel(AlphaModel):
-    def update(self, algorithm: QCAlgorithm, data: Slice) -> list[Insight]:
-        insights = []
-
-        for symbol in data.keys():  # type: ignore[reportCallIssue]
-            direction = self._choice_random_direction()
-            insight = Insight.price(symbol, timedelta(days=5), direction)
-            insights.append(insight)
-
-        self._log_insights(algorithm, insights)
-
-        return insights
-
-    def _choice_random_direction(self) -> InsightDirection:
-        direction = random.choices(
-            [InsightDirection.UP, InsightDirection.DOWN, InsightDirection.FLAT],
-            weights=[0.5, 0.5, 0],
-        )[0]
-
-        return direction
-
-    def _log_insights(self, algorithm: QCAlgorithm, insights: list[Insight]) -> None:
-        algorithm.debug(f"{algorithm.time}: === Insights ===")
-        for insight in insights:
-            algorithm.debug(
-                f"Symbol: {insight.symbol}, "
-                f"Direction: {insight.direction}, "
-                f"Duration: {insight.period}, "
-            )
+        self.add_alpha(RandomAlphaModel(period=3, log_insights=True))
